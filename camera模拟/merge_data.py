bcbd8af7b5e119ce25c8af45aec8a98f34d5ce15
chenq <chenqiang97@foxmail.com>
2024-04-22 23:35:54 +0800

    camera运动仿真 往场景中添加数据

--- camera模拟/merge_data.py
+++ camera模拟/merge_data.py
@@ -6,8 +6,14 @@ import shutil
 import zipfile
 
 
-def unzip_and_merge_to_folders(zip_filepath, dest_folder):
-    # 解压zip文件到目标文件夹
+def unzip_and_merge_to_folders(zip_filepath, dest_folder, is_update=False):
+    """
+    解压zip文件到目标文件夹，并合并到子文件夹中
+    :param zip_filepath:
+    :param dest_folder:
+    :param is_update: 是否更新，如果为True，则删除目标文件夹中的同名文件夹，并将解压后的文件夹移动到目标文件夹中
+    :return:
+    """
     # 检查目标文件夹是否存在，如果不存在则创建
     if not os.path.exists(dest_folder):
         os.makedirs(dest_folder)
@@ -23,18 +29,26 @@ def unzip_and_merge_to_folders(zip_filepath, dest_folder):
         # 构造目标子文件夹的完整路径
         if not os.path.exists(os.path.join(dest_folder, dir)):
             shutil.move(os.path.join(temp_dir, dir), dest_folder)
-            # 此处将记录写入mongodb数据库，待完成
+        else:
+            if is_update:
+                shutil.rmtree(os.path.join(dest_folder, dir))  # 如果目标子文件夹已存在，则删除它
+                shutil.move(os.path.join(temp_dir, dir), dest_folder)  # 移动解压后的文件夹到目标文件夹
+            else:
+                print(f"目标子文件夹{dir}已存在，跳过")
+
+
 
 def check_txt_and_folder(folder_path):
     # 检查txt文件和对应的文件夹是否存在
     for root, dirs, files in os.walk(folder_path):
         for file in files:
-            if file.endswith(".txt"):
-                txt_path = os.path.join(root, file)
-                dir_path = txt_path.replace(".txt", "")
-                if not os.path.exists(dir_path):
-                    print(f"txt文件{txt_path}对应的文件夹{dir_path}不存在")
-                    return False
+            if not file.endswith(".txt"):
+                continue
+            txt_path = os.path.join(root, file)
+            dir_path = txt_path.replace(".txt", "")
+            if not os.path.exists(dir_path):
+                print(f"txt文件{txt_path}对应的文件夹{dir_path}不存在")
+                return False
 # # 使用函数
 # zip_filepath = "Crawl.zip"  # 压缩文件的路径
 # dest_folder = r"D:\workspace\ellie\camera模拟\data_folders\Crawl"  # 之前创建的包含子文件夹的目录
